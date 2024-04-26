863e4f3d65f2e3e66d64ced786b60fe365ab0e02
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-04-26 14:55:24 +0500

    ShouldThrowValidationExceptionOnAddIfPersonIsNullAndLogItAsync -> FAIL

---
+++ eXChanger.POC.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.Validation.Add.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit;
+using eXChanger.POC.Models.Foundations.Persons;
+using eXChanger.POC.Api.Models.Foundations.Persons.Exceptions;
+
+namespace eXChanger.POC.Tests.Unit.Services.Foundations.Persons
+{
+	public partial class PersonServiceTests
+	{
+		[Fact]
+		public async Task ShouldThrowValidationExceptionOnAddIfPersonIsNullAndLogItAsync()
+		{
+			// given
+			Person nullPerson = null;
+			NullPersonException nullPersonException = new();
+
+			PersonValidationException expectedPersonValidationException =
+				new(nullPersonException);
+
+			// when
+			ValueTask<Person> addPersonTask =
+				this.personService.AddPersonAsync(nullPerson);
+
+			// then
+			await Assert.ThrowsAsync<PersonValidationException>(() =>
+			  addPersonTask.AsTask());
+
+			this.loggingBrokerMock.Verify(broker =>
+				broker.LogError(It.Is(SameExceptionAs(expectedPersonValidationException))),
+				Times.Once());
+			this.storageBrokerMock.Verify(broker =>
+			 broker.AddPersonAsync(It.IsAny<Person>()), Times.Never);
+
+			this.loggingBrokerMock.VerifyNoOtherCalls();
+			this.storageBrokerMock.VerifyNoOtherCalls();
+		}
+	}
+}
---
+++ eXChanger.POC.Tests.Unit/Services/Foundations/Persons/PersonServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using eXChanger.POC.Api.Brokers.Loggings;
+using eXChanger.POC.Brokers.Storages;
+using eXChanger.POC.Services.Foundations.Persons;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace eXChanger.POC.Tests.Unit.Services.Foundations.Persons
+{
+	public partial class PersonServiceTests
+	{
+		private readonly Mock<IStorageBroker> storageBrokerMock;
+		private readonly Mock<ILoggingBroker> loggingBrokerMock;
+		private readonly IPersonService personService;
+
+		public PersonServiceTests(Mock<IStorageBroker> storageBrokerMock, Mock<ILoggingBroker> loggingBrokerMock, IPersonService personService)
+		{
+			this.storageBrokerMock = storageBrokerMock;
+			this.loggingBrokerMock = loggingBrokerMock;
+			this.personService = personService;
+		}
+
+		private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+			actualException => actualException.SameExceptionAs(expectedException);
+		
+	}
+}
